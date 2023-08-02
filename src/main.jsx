03e55d544710fbc2277ad20852dd670280ef64c0
Reno Angelo <personal.renoangelo@gmail.com>
2023-08-02 19:34:27 +0800

    #1 removed extra comma

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { PAGES } from './routes';
 import index from './index.css';
-  
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {PAGES.map((item) => item.component)}
-  </React.StrictMode>,
+  </React.StrictMode>
 )
 
 
