4425e1cff12ee7d9f7aa72bfeedfbf65baf8cd39
Reno Angelo <personal.renoangelo@gmail.com>
2023-08-13 23:05:02 +0800

    refs #11 added home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
 import React, { Fragment } from 'react';
 import { NavbarComponent } from '../components/NavbarComponent';
 import { HeroSectionPage } from './HeroSectionPage';
+import { AboutPage } from './AboutPage';
+import WorkPage from './WorkPage';
+import SkillsPage from './SkillsPage';
+import ContactPage from './ContactPage';
 
 const HomePage = () => {
   return (
     <Fragment>
       <NavbarComponent />
       <HeroSectionPage />
-
-
+      <AboutPage />
+      <WorkPage />
+      <SkillsPage />
+      <ContactPage />
     </Fragment>
   );
 }
